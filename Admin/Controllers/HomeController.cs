b2c865ca0dffd03b88cf07fee780d0aa688f9113
Tommy Dang <dtranthuy1995@gmail.com>
2026-06-16 10:53:29 +0700

    add view

--- Admin/Controllers/HomeController.cs
+++ Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using Admin.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Admin.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
